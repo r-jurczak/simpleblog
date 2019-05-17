4c0b142baf1f366cf7b7dc8b2ac1585e63f292ee
r-jurczak <radoslawjurczak95@gmail.com>
2019-05-17 15:56:56 +0200

    Add post_list view
    Signed-off-by: r-jurczak <radoslaw.jurczak@duffandphelps.com>

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    """View for a list of posts."""
+    posts = Post.published.all()
+    return render(request, template_name='blog/post/list.html', context={'posts': posts})
